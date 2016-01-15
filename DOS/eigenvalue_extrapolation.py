b704a6ec5358e336abbb888eb18c99ab3b0dbe40
mdgratale <goatcrossing123@yahoo.com>
2016-01-15 14:59:53 -0500

    account for finite sampling times of eigenmodes

--- DOS/eigenvalue_extrapolation.py
+++ DOS/eigenvalue_extrapolation.py
@@ -2,20 +2,21 @@
 """
 Created on Sat Nov 14 10:08:08 2015
 
-Extrapolate eigenvalues to infinite time, currently only written for 3 time frame inputs
-
-Inputs:  
-eg1 = first set of eigenvalues (longest time)
-eg2 = second set of eigenvalues (second longest time)
-eg3 = third set of eigenvalues (next longest time)
-eg4 = fourth set of eigenvalues (shortest time)
-t1,t2,t3,t4 = number of frames for each set of eigenvalues           
-
 @author: mgratale
 """
 
 def eigenvalue_extrapolation(eg1,eg2,eg3,eg4,t1,t2,t3,t4):
-    
+    """
+    Extrapolate eigenvalues to infinite time, currently only written for 4 time frame inputs
+
+    Inputs:  
+    eg1 = first set of eigenvalues (longest time)
+    eg2 = second set of eigenvalues (second longest time)
+    eg3 = third set of eigenvalues (next longest time)
+    eg4 = fourth set of eigenvalues (shortest time)
+    t1,t2,t3,t4 = number of frames for each set of eigenvalues           
+
+    """    
     nfreq=len(eg1) #number of frequencies/degrees of freedom
     times=np.array([t1,t2,t3,t4])
     R=float(nfreq)/times
